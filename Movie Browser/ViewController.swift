2ea3f953cf75ba588516fbe991c1d0e001a1e293
BudhabhooshanPatil <patil.bhooshan@outlook.com>
2019-04-08 16:47:45 +0530

    Initial Commit

---
+++ Movie Browser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie Browser
+//
+//  Created by Budhabhooshan Patil on 08/04/19.
+//  Copyright © 2019 Inscripts.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
